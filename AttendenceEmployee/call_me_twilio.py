74be6361bed1510ace93d619249301531395d030
Shubham Mishra <49332766+Shubham11012001@users.noreply.github.com>
2020-07-07 02:06:16 +0530

    Update call_me_twilio.py

--- AttendenceEmployee/call_me_twilio.py
+++ AttendenceEmployee/call_me_twilio.py
@@ -5,8 +5,8 @@ from twilio.rest import Client
 def calling_user():
     # Your Account Sid and Auth Token from twilio.com/console
     # DANGER! This is insecure. See http://twil.io/secure
-    account_sid = 'AC42105d199d3a0a6cf7459457968fb55d'
-    auth_token = '0e4be08660b87383a9af231655a26cb4'
+    account_sid = 'your key'
+    auth_token = 'your token'
     client = Client(account_sid, auth_token)
 
     call = client.calls.create(
